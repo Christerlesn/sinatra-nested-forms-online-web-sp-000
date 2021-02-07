c6348a8bdebc4bc058944e0f08c5c3a48f9d0fb3
Christerle_Nounoute <christerlesn@gmail.com>
2021-02-06 21:53:57 -0800

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+    attr_reader :name, :type, :booty
+
+    SHIPS = []
+
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:type]
+        @height = args[:booty]
+
+        @@all << self
+    end
+
+    def self.all
+        SHIPS
+    end
+
+    def self.destroy
+        @@all.clear
+    end
 end
